4f2ddc223dd37ca62850634c60981598327a52f2
Daniel Rodrigues <daniel.rodrigues46@fatec.sp.gov.br>
2024-10-14 18:27:51 +0000

    main

--- SistemaAcademico/src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
-insert into tb_aluno(data_nascimento, email,endereco,nome_aluno,periodo) values("2009-06-16","luis@teste.com","Rua Melo Peixoto","Luis Felipe","Diurno");
-insert into tb_aluno(data_nascimento, email,endereco,nome_aluno,periodo) values("2001-10-31","pedro@teste.com","Rua Restinga","Pedro Henrique","Vespertino");
-insert into tb_aluno(data_nascimento, email,endereco,nome_aluno,periodo) values("2000-04-09","erica@teste.com","Avenida Celso Garcia","Erica Silva","Noturno");
-insert into tb_aluno(data_nascimento, email,endereco,nome_aluno,periodo) values("1990-07-10","duda@teste.com","Alamenda Santos","Maria Eduarda","Noturno");
-insert into tb_aluno(data_nascimento, email,endereco,nome_aluno,periodo) values("1950-03-18","gabriel@teste.com","Rua do Programador","Gabriel Lima","Diurno");
